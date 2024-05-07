86b345b79e4f4d0bc082d991bd8084e2bb057e00
Roy <rascojohnroy47@gmail.com>
2024-05-07 15:08:57 +0800

    Final Output Integrative

--- JS/login.js
+++ JS/login.js
@@ -1,10 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-
-import { getDatabase, get, ref, child} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
-
+import { getDatabase, ref, get, child} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
 import { getAuth, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
- 
+
 const firebaseConfig = {
   apiKey: "AIzaSyDD2iT8hKifwRuaHv8rVCWW2HebVai24go",
   authDomain: "cnscsportstallysystem.firebaseapp.com",
@@ -19,42 +17,38 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 const auth = getAuth(app);
-const dbref = ref (db);
-
-// Selecting input elements
-let Email = document.getElementById('InputEmail1');
-let Password = document.getElementById('InputPassword1');
-let MainForm = document.getElementById('MainForm');
-
-// Function to register a user
+const dbref = ref(db);
+// Function to sign in a user
 let SignInUser = evt => {
     evt.preventDefault();
-
    
     const emailValue = Email.value;
     const passwordValue = Password.value;
 
-    // Registering the user with email and password
+    // Sign in the user with email and password
     signInWithEmailAndPassword(auth, emailValue, passwordValue)
         .then((credentials) => {
-            get(child(dbref, 'UsersAuthList/'+ credentials.user.uid)).then((snapshot)=>{
-                if(snapshot.exists){
+            // Get the reference to the user node in the database
+            const userRef = ref(db, 'UsersAuthList/' + credentials.user.uid);
+            
+            // Retrieve user data
+            get(userRef).then((snapshot) => {
+                if (snapshot.exists()) {
                     sessionStorage.setItem("user-info", JSON.stringify({
                         firstname: snapshot.val().firstname,
                         lastname: snapshot.val().lastname
-                    }))
+                    }));
                     sessionStorage.setItem("user-creds", JSON.stringify(credentials.user));
+                    // Redirect to admin.html after successful login
                     window.location.href = 'admin.html';
-                    
                 }
-            }) 
+            }).catch((error) => {
+                console.error("Error getting user data:", error);
+            });
         })
         .catch((error) => {
             alert(error.message);
-            console.log(error.code);
-            console.log(error.message);
+            console.error("Error signing in:", error);
         });
 };
 
-// Adding event listener to the form
-MainForm.addEventListener('submit', SignInUser);  
